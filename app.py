0561ec2b6e8c5ba1b0ef6394d255d5d51baf6828
Amruta Kajale <amrutakajale126@gmail.com@gmail.com>
2026-02-12 08:33:15 +0530

    Set up complete login workflow: connected MySQL database, validated user credentials, implemented session-based authentication, and added dashboard & logout routes

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import mysql.connector
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template, request,redirect,url_for, session
 
 app = Flask(__name__)
+
+app.secret_key = "secret123"
+
 
 #Database connection
 db=mysql.connector.connect(
@@ -23,16 +27,35 @@ def login():
         email=request.form["email"]
         password=request.form["password"]
 
-        query = "SELECT * FROM users WHERE email=%s AND password=%s"
-        cursor.execute(query, (email,password))
+        cursor.execute(
+         "SELECT * FROM users WHERE email=%s AND password=%s",
+     (email,password)
+        )
         user = cursor.fetchone()
 
         if user:
-            return "Login Successful !"
+            session["user"]=user[1]
+            return redirect(url_for("dashboard"))
         else:
             return "Invalid email or password"
         
     return render_template("login.html")
+
+#Dashboard route add
+@app.route("/dashboard")
+def dashboard():
+    if "user" in session:
+        return f"Welcome {session['user']} to dashboard"
+    else:
+        return redirect(url_for("login"))
+    
+
+#Logout route add
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
