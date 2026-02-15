cd0d2435ed697f05687f6c18b545ba9dd7e8a68d
Amruta Kajale <amrutakajale126@gmail.com@gmail.com>
2026-02-15 20:09:44 +0530

    Updated database schema: added foreign keys for student and faculty linkage

---
+++ database_changes.sql
@@ -0,0 +1,8 @@
+ALTER TABLE attendance ADD COLUMN student_id INT;
+ALTER TABLE attendance ADD COLUMN marked_by INT;
+
+ALTER TABLE attendance
+ADD FOREIGN KEY (student_id) REFERENCES students(id);
+
+ALTER TABLE attendance
+ADD FOREIGN KEY (marked_by) REFERENCES users(id);
